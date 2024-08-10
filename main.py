5312484a79d09531b3dd159e5e814d2ccaba6690
Ihmaid <gabriel-ihmaid@ufrrj.br>
2024-08-10 15:39:17 -0300

    displayed the table, day 46

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget,\
     QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -19,6 +20,10 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
 
         self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name",
+                                             "Course", "Mobile"))
+        self.setCentralWidget(self.table)
 
     def load_data(self):
         self.table
